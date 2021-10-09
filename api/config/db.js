464b755cad8967b0f4d4cd47d62b0bfc54cd3d8f
cmzluna <claudiomazzoli@gmail.com>
2021-10-09 19:34:39 -0300

    heroku

--- api/config/db.js
+++ api/config/db.js
@@ -1,5 +1,14 @@
 const { Sequelize } = require("sequelize");
 
+// checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+  });
+}
+
 const sequelize = new Sequelize("omdb", null, null, {
   host: "localhost",
   dialect: "postgres",
