93e55f1fb9d897834b708d7420a2603e9346b8b5
cmzluna <claudiomazzoli@gmail.com>
2021-10-10 11:29:14 -0300

    heroku

--- api/config/db.js
+++ api/config/db.js
@@ -1,18 +1,21 @@
+require("dotenv");
 const { Sequelize } = require("sequelize");
 
 // checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
-  });
-}
 
-const sequelize = new Sequelize("omdb", null, null, {
-  host: "localhost",
+// if (process.env.DATABASE_URL) {
+// the application is executed on Heroku ... use the postgres database
+const sequelize = new Sequelize(process.env.DATABASE_URL, null, null, {
   dialect: "postgres",
-  logging: false,
+  protocol: "postgres",
 });
+
+// } else {
+//   const sequelize = new Sequelize("omdb", null, null, {
+//     host: "localhost",
+//     dialect: "postgres",
+//     logging: false,
+//   });
+// }
 
 module.exports = sequelize;
--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 // server configs
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
